9714d3dab7af48d5de49365676211d4ca0df2e6a
RAssisBig <risilvas66@gmail.com>
2022-07-08 10:09:33 -0300

    adição do sistema módulos para execução do login

---
+++ js/moduloDados.js
@@ -0,0 +1,27 @@
+const userLogin = [
+{
+    id: 1001,
+    userName: "victoricoma",
+    senha: "gordinho123",
+email:"victor.icoma@gmail.com"
+},
+{
+    id: 1002,
+    userName: "tatianeassis",
+    senha: "ja123456",
+email:"janasilva@gmail.com"
+},
+{
+    id: 1003,
+    userName: "marcospinheiro",
+    senha: "m123456",
+email: "pinheiromarcos@gmail.com"
+},
+{
+    id: 1004,
+    userName: "elisamarcondes",
+    senha: "123456",
+email:"elimarcondes@gmail.com"
+}
+];
+export default userLogin
